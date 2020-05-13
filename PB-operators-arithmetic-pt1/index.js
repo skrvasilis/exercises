b9d482567060957e0a3c74694b6fd1fe6ea27128
skrvasilis <skrvasilis@gmail.com>
2020-05-13 14:03:10 +0200

    done

---
+++ PB-operators-arithmetic-pt1/index.js
@@ -0,0 +1,45 @@
+let x=2.5;
+let y=12;
+
+
+// 1.
+console.log(x+y);
+// 2.
+console.log(y-x);
+console.log(y-x);
+// 3.
+console.log(x*y);
+// 4.
+console.log(x/y);
+
+// 5.
+let z=10;
+let resultOne = ((x*y)/z);
+console.log(resultOne);
+
+// 6.
+let a=15;
+let b=9;
+console.log(a/b);
+
+// 7.
+
+let c=20;
+let resultTwo= ((a+b)*c);
+console.log(resultTwo);
+
+// 8.
+console.log(a++);
+
+// 9.
+console.log(b--);
+
+
+// 10.
+let d; 
+d = b-a;
+console.log(d);
+console.log(d + c);
+
+// 11.
+console.log(resultOne/resultTwo);
