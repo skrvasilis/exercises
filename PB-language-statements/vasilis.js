f67ce7df4b4ebfc21dc590d701834b6211c60295
skrvasilis <skrvasilis@gmail.com>
2020-05-13 09:28:40 +0200

    done

---
+++ PB-language-statements/vasilis.js
@@ -0,0 +1,13 @@
+console.log('Hello World.';
+
+
+// console.log('This is my first js file.');
+
+
+
+
+/*  1. my statement doesn't print
+ */
+ 
+ /* 2. SyntaxError: missing ) after argument list  */
+
