c504222725ce40916f979c1585d1502095f190f7
skrvasilis <skrvasilis@gmail.com>
2020-05-14 11:22:15 +0200

    done

---
+++ 03-PB-console/PB_console.js
@@ -0,0 +1,25 @@
+// 1.
+console.log('this is an exercise logging');
+
+// 2.
+let myVariable = 'This is the value';
+console.log(myVariable);
+
+//3 .
+let firstName = 'Vasilis';
+let lastName = 'Skrimpas';
+console.log(firstName, lastName);
+
+//4.
+let name = 'Max';
+let city = 'Berlin';
+let age = 34;
+let job = 'Teacher';
+let gender = 'male';
+console.log(name, city, age, job, gender);
+
+// 5.
+console.log(name + ' is a ' + age + ' old ' + gender + ' ' + job + ' who lives in ' + city);
+
+// trying different things
+console.log(name + ' lives in ' + city + '. He is ' + age + ' years old and he works as a ' + job + '.He is ' + gender);
