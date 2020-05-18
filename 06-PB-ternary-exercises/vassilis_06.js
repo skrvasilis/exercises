9437b2168820500dfd3c3d495eb525db3d0e9726
skrvasilis <skrvasilis@gmail.com>
2020-05-18 10:25:38 +0200

    06 is done

---
+++ 06-PB-ternary-exercises/vassilis_06.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// 1.
+let isDog = true;
+console.log(isDog === true ? 'pat,pat' : 'Find me a dog to pat!');
+
+// 2.
+let speedCheck = 60;
+let speedLimit = 50;
+console.log(speedCheck > speedLimit ? 'you\'re\ going too fast!' : 'You\'re\ driving below the speed limit,Oma'); 
+
+// 3. 
+let age = 12;
+console.log(age < 16 ? 'serve butter beer' : 'serve beer');
+
+// 4.
+let isStudent = false;
+console.log(isStudent === true ? 'Ticket costs \u20AC5.00': 'Ticket costs \u20AC12.00');
+
+// 5.
+let okMarie = 'cake';
+console.log(okMarie === 'cake' ? 'Let them eat cake' : 'Oh,bother');
