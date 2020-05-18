67599d0f226234c1c105ff0907164671a1af9736
skrvasilis <skrvasilis@gmail.com>
2020-05-18 11:19:26 +0200

    done after the comments

--- 06-PB-ternary-exercises/vassilis_06.js
+++ 06-PB-ternary-exercises/vassilis_06.js
@@ -2,21 +2,26 @@
 
 // 1.
 let isDog = true;
+console.log('Here we check if isDog is true or not.');
 console.log(isDog === true ? 'pat,pat' : 'Find me a dog to pat!');
 
 // 2.
 let speedCheck = 60;
 let speedLimit = 50;
-console.log(speedCheck > speedLimit ? 'you\'re\ going too fast!' : 'You\'re\ driving below the speed limit,Oma'); 
+console.log('Here we check if speedCheck is bigger than speedLimit.');
+console.log(speedCheck > speedLimit ? 'you\'re going too fast!' : 'You\'re driving below the speed limit,Oma'); 
 
 // 3. 
 let age = 12;
+console.log('Here we check if age is less the 16.');
 console.log(age < 16 ? 'serve butter beer' : 'serve beer');
 
 // 4.
 let isStudent = false;
+console.log('Here we check if isStudent is true or false.');
 console.log(isStudent === true ? 'Ticket costs \u20AC5.00': 'Ticket costs \u20AC12.00');
 
 // 5.
 let okMarie = 'cake';
-console.log(okMarie === 'cake' ? 'Let them eat cake' : 'Oh,bother');
+console.log('Here we check if okMarie is \'cake\' or not.');
+console.log(okMarie === 'cake' ? 'Let them eat cake' : 'Oh,bother'); 
