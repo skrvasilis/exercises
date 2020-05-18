14b071161882b6898866d90eacb1a7f0b249eada
skrvasilis <skrvasilis@gmail.com>
2020-05-18 14:13:25 +0200

    07 is done

---
+++ 07-PB-data-types-pt1/vassilis_07.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// 1.
+let myVariable = 'inspiration';
+console.log(myVariable);
+
+// 2. 
+console.log('The 2nd and the 6th character of inspiration');
+console.log(myVariable[1] + myVariable[5]);
+
+// 3. 4. 5.
+let name = 'Adam';
+let age = 48;
+let year = 2043;
+
+// 6. 
+console.log(`${name} is ${age} in ${year}`);
+
+// 7. 
+let isMarried = 'false';
+
+// 8.
+console.log(`${name} is married:${isMarried}`);
+
+// 9.
+let programmingLanguage = 'javaScript';
+let isFun = true;
+console.log(`${programmingLanguage} is fun:${isFun}`);
+
+// 10.
+console.log('the j and the S from javaScript');
+console.log(programmingLanguage[0] + programmingLanguage[4]);
+
+// 11.
+console.log('the length of javaScript is:');
+console.log(programmingLanguage.length);
