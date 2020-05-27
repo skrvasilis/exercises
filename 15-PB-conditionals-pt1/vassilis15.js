e36ade4fb5a33130672c5fa26525408edd1a36b7
skrvasilis <skrvasilis@gmail.com>
2020-05-27 09:50:35 +0200

    15 is done

--- 15-PB-conditionals-pt1/vassilis15.js
+++ 15-PB-conditionals-pt1/vassilis15.js
@@ -1,21 +1,25 @@
 'use strict';
 
 // 1.
-let x = 50;
+let x = 55;
 let y = 4;
-if (50 <= x && x <= 99 || 50 <= y && y <= 99) {
+if (50 <= x <= 99 || 50 <= y <= 99) {
     console.log(true);
 }
 
 // 2.
 let z = null;
-if (50 <= x && x <= 99 || 50 <= y && y <= 99 || 50 <= z && z <= 99) {
+if (50 <= x <= 99 || 50 <=  y <= 99 || 50 <= z <= 99) {
     console.log(true);
 }
 // 3. 
 let a = 1;
 let b = 1;
 let c = 33;
+/* 
+ if (Math.max(a , b , c) === a) {
+     console.log(true);
+ } */
 if (a !== b && a !== c && b !== c) {
     if (a > b) {
         if (a > c) {
@@ -40,12 +44,14 @@ if (a !== b && a !== c && b !== c) {
 let myString = 'python';
 let mySecondString = 'Py' + myString;
 let test = mySecondString.substring(0, 2);
+// console.log(mySecondString.startsWith('Py'[0,2]));
+
 if (test = 'Py') {
     console.log(myString);
 }
 // 5.
 let numberA = 8;
-let numberB = 8;
+let numberB = 7;
 if (numberA + numberB >= 50 && numberA + numberB <= 80) {
     console.log(65);
 } else {
@@ -59,7 +65,7 @@ if (numberA + numberB === 8 || numberA - numberB === 8) {
 
 // 7.
 if (numberA === 15 || numberB === 15 || numberA + numberB === 15) {
-    console.log(true);
+    console.log(`15 ${true}`);
 }
 // 8. 
 if (numberA % 7 === 0 || numberA % 11 === 0 || numberB % 7 === 0 || numberB % 11 === 0) {
@@ -72,7 +78,7 @@ if (numberA === numberB) {
 }
 //10. 
 let difference = numberA - 19;
-if (difference === 19) {
+if (numberB > 19) {
     console.log(difference * 2);
 }
 // 11.
