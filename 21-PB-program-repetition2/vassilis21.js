bc8d810215029ec106c436be3771e2e19d949152
skrvasilis <skrvasilis@gmail.com>
2020-06-02 15:31:38 +0200

    21dne

--- 21-PB-program-repetition2/vassilis21.js
+++ 21-PB-program-repetition2/vassilis21.js
@@ -2,7 +2,7 @@
 
 // 1.
 let i = 0;
-while (i<15) {
+while (i < 15) {
     console.log(i);
     i++;
 }
@@ -10,15 +10,16 @@ while (i<15) {
 // 2. 
 let num = 1;
 let mySum = 0;
-while (num <=20) {
-    mySum = mySum+num;
+while (num <= 20) {
+    mySum = mySum + num;
     console.log(mySum);
     num++;
 }
 
+
 // 3.
-let num3 =0;
+let num3 = 0;
 do {
     console.log('the number is ', num3);
     num3++;
-} while (num3 <20)
+} while (num3 < 20)
