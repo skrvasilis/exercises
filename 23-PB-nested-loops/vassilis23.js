6c494e56fa4bfea260a0050f3132323c90736529
skrvasilis <skrvasilis@gmail.com>
2020-06-04 11:26:35 +0200

    no bonus yet

--- 23-PB-nested-loops/vassilis23.js
+++ 23-PB-nested-loops/vassilis23.js
@@ -1,12 +1,10 @@
-for (let i = 1; i<=10; i++) {
+for (let i = 1; i <= 10; i++) {
     let space = '';
-    for (let j = 0; j<i-1; j++) {
+    for (let j = 0; j < i - 1; j++) {
         space += ' ';
     }
-    for (let k = 21; k>(i*2); k--) {
-    space += '*';
+    for (let k = 21; k > (i * 2); k--) {
+        space += '*';
     }
     console.log(space);
 }
-
-
