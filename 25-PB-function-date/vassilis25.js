053b8e4a53212470a83a1e7d293936522325b415
skrvasilis <skrvasilis@gmail.com>
2020-06-08 13:01:42 +0200

    25 finished

--- 25-PB-function-date/vassilis25.js
+++ 25-PB-function-date/vassilis25.js
@@ -5,12 +5,16 @@ const eventFunction = (today, givenDate) => {
         let day = givenDate.getDate();
         let month = givenDate.getMonth() + 1;
         let year = givenDate.getFullYear();
-        console.log(`Your book to the event for the ${day}-${month}-${year} is already guaranteed!`);
+        console.log(`Your book to the event for the ${day}/${month}/${year} is already guaranteed!`);
+        // easier way to display the date
+        console.log(`Your book to the event for the ${givenDate.toLocaleDateString('en-GB')} is already guaranteed!`);
     } else {
         console.log('please select a future date.');
     }
 }
 
-const userDate = new Date('2020-10-1');
+const userDate = new Date('2020-10-21');
 const currentDay = new Date();
 eventFunction(currentDay, userDate);
+// second date with no variables
+eventFunction(new Date(), new Date('2020-22-5'));
