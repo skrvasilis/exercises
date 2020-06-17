655c441fdd1c72c22bdcab3443653759f98dec95
skrvasilis <skrvasilis@gmail.com>
2020-06-17 11:25:06 +0200

    32 done

---
+++ 32-PB-datastructure-array/vassilis32.js
@@ -0,0 +1,13 @@
+'use strict'
+// 1.
+
+const myArray = [123, 'vasilis', 'hello', 13, true];
+console.log(myArray);
+
+// 2.
+const thirdElement = myArray[2];
+console.log(thirdElement);
+
+// 3.
+myArray[3] = 'fourth element';
+console.log(myArray);
