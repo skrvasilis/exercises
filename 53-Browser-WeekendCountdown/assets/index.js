0ca5c514a32c405dcef6bd878581501f7e4a9d46
skrvasilis <skrvasilis@gmail.com>
2020-07-15 10:41:24 +0200

    53 done

--- 53-Browser-WeekendCountdown/assets/index.js
+++ 53-Browser-WeekendCountdown/assets/index.js
@@ -2,12 +2,8 @@ let inp = document.querySelector('input')
 let button = document.querySelector('button')
 let h2 = document.querySelector('h2')
 myDate = new Date()
-console.log(myDate)
 myDay = myDate.getDay()
 let weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-console.log(weekDay[myDay])
-console.log(weekDay.indexOf('Saturday'))
-console.log(weekDay.indexOf(weekDay[myDay]))
 dayLeft = weekDay.indexOf('Saturday') - weekDay.indexOf(weekDay[myDay])
 
 
